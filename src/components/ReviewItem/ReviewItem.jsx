5ec5341e7b1c496b717d4e960df89a923c2de797
Programming Hero <programming.hero1@gmail.com>
2023-04-02 22:48:38 -0500

    style review item

--- src/components/ReviewItem/ReviewItem.jsx
+++ src/components/ReviewItem/ReviewItem.jsx
@@ -1,11 +1,21 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faTrashAlt } from '@fortawesome/free-solid-svg-icons'
 import './ReviewItem.css';
 
-const ReviewItem = ({product}) => {
-    const {id, img, price, name, quantity} = product;
+const ReviewItem = ({ product }) => {
+    const { id, img, price, name, quantity } = product;
     return (
         <div className='review-item'>
             <img src={img} alt="" />
+            <div className='review-details'>
+                <p className='product-title'>{name}</p>
+                <p>Price: <span className='orange-text'>${price}</span></p>
+                <p>Order Quantity: <span className='orange-text'>{quantity}</span></p>
+            </div>
+            <button className='btn-delete'>
+                <FontAwesomeIcon className='delete-icon' icon={faTrashAlt} />
+            </button>
         </div>
     );
 };
